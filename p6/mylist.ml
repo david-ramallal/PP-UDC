d0931b2accfc4c51802808658f38c0fc8d16f1be
David García Ramallal <91455051+david-ramallal@users.noreply.github.com>
2021-10-29 09:18:22 +0200

    P6 añadida final

--- p6/mylist.ml
+++ p6/mylist.ml
@@ -10,28 +10,11 @@ let rec length = function
 	[] -> 0
 	| _::t -> 1 + length t
 	
-(*
-let length l =
-	let rec aux i = function
-		[] -> i
-		| _::t -> aux (i+1) t
-	in
-		aux 0 l
-*)
-	
 let rec compare_lengths l1 l2 = match l1, l2 with   
 	[], [] -> 0
   | [], _  -> -1
   | _,  [] -> 1
   | _::t1, _::t2 -> compare_lengths t1 t2
-  
-(*
-let rec compare_lengths = function 
-	[] -> (function [] -> 0
-					| _-> (-1))
-	| _::t -> (function [] -> 1
-					| _::t2 -> compare_lengths t t2) 
-*)
 
 let rec nth l n = 
 	if n < 0 then raise(Invalid_argument "nth")
@@ -42,15 +25,9 @@ let rec nth l n =
 						| n -> nth t (n-1))
 	in aux l n
 	
-let rec append  = function  
-	[] -> (function l -> l)
-	| h::t -> (function l -> h :: append t l)
-	
-(*
 let rec append l1 l2 = match l1 with  
 	[] -> l2
 	| h::t -> h :: append t l2
-*)
 
 let rec find f l = match l with
 	[] -> raise(Not_found)
@@ -91,20 +68,21 @@ let rec combine l1 l2 = match (l1, l2) with
 	|(h1::t1),(h2::t2) -> (h1, h2)::combine t1 t2
 	|(_,_) -> raise(Invalid_argument"combine")
 	
-let rev l =
-  let rec aux n = function
-    [] -> n
-    |h::t -> aux (h::n) t
-  in aux [] l
+let rec rev  = function
+    [] -> []
+    | h::t -> rev t @ [h]              
 	
 let init n f =
   if n < 0
     then raise (Invalid_argument"len < 0")
     else let rec aux (i, l) =
       if i = n then l else aux(i + 1, f i::l)
-    in rev(aux(0, []));;
+    in rev(aux(0, []))
 
-let rev_append l1 l2 = append (rev l1) l2
+let rec rev_append l1 l2 = match l1 with
+	[] -> l2
+	| h::t -> rev_append t (h::l2)
+
 
 let rec concat l = match l with
 	[] -> []
@@ -121,15 +99,15 @@ let rev_map f l= rev (map f l)
 let rec map2 f l1 l2 =
   if (length l1 <> length l2)
     then raise (Invalid_argument "map2") else if (length l1 == 0)
-    then [] else (f(hd l1)(hd l2))::map2 f (tl l1)(tl l2);;
+    then [] else (f(hd l1)(hd l2))::map2 f (tl l1)(tl l2)
 
-let rec fold_left f g l = match l with
-  [] -> g
-  |h::t -> fold_left f (f g h) t;;
+let rec fold_left f a = function
+  [] -> a
+  | h::t -> fold_left f (f a h) t
 
-let rec fold_right f l g = match l with
-  [] -> g
-  |h::t -> f h (fold_right f t g);;
+let rec fold_right f l b = match l with
+  [] -> b
+  | h::t -> f h (fold_right f t b)
 
 
 
