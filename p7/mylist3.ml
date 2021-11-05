6d578ad90627bcbae82daf974430c44cd97968e9
David García Ramallal <91455051+david-ramallal@users.noreply.github.com>
2021-11-05 11:59:09 +0100

    p7 terminada

--- p7/mylist3.ml
+++ p7/mylist3.ml
@@ -4,12 +4,25 @@ let rec remove n l = match l with
 
 let rec remove_all n l = match l with
 	[] -> []
-	| h::t -> if n = h then remove n t else h::remove n t
+	| h::t -> if n = h then remove_all n t else h::remove_all n t
 	
-let rec lprod l1 l2 = match l1, l2 with
-	[],[] -> []
-	|(h1::t1),(h2::t2) -> (h1,t2) :: lprod l1 t2
-	| 
+let rec ldif l1 l2 = match l2 with
+	[] -> l1
+	| h::t -> ldif (remove_all h l1) t
+
+let rec aux_lprod n l = match l with
+	[] -> []
+	| h::t -> (n, h)::aux_lprod n t
+	
+let rec lprod l1 l2 = match l1 with
+	[] -> []
+	| h::t -> List.append (aux_lprod h l2) (lprod t l2)
+
+let rec divide = function
+	[] -> ([],[])
+	| h1::h2::t -> let  (l1, l2) = divide t in (h1::l1,h2::l2)
+	| l -> l, []
+
 	
 
 	
