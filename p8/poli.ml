0c187ab41bdafdd05941a98651bdc32e7aca5db7
David García Ramallal <91455051+david-ramallal@users.noreply.github.com>
2021-11-05 18:27:48 +0100

    p8 terminada

--- p8/poli.ml
+++ p8/poli.ml
@@ -1,1 +1,14 @@
-
+let f x = x
+let h (x,y) = x
+let i (x,y) = y
+let j x = [x]
+(*
+No existe un número máximo de funciones de un tipo
+que se pueden escribir, aunque es cierto que en los tres 
+primeros casos las posibles funciones son formas diferentes 
+de escribir la misma. Por ejemplo:
+let f2 x = fst (x,x)
+let h2 = fst
+let i2 = snd 
+let j2 x = [x;x;x]
+*)
