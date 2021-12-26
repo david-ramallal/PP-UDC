ebf89b55e3153defc551fe075f89997518b73f0b
David García Ramallal <91455051+david-ramallal@users.noreply.github.com>
2021-12-26 17:46:47 +0100

    p12 terminada

--- p12/breadth_first.ml
+++ p12/breadth_first.ml
@@ -2,15 +2,10 @@ let rec breadth_first = function
 	Gt (x, []) -> [x]
 	| Gt (x, (Gt (y, t2))::t1) -> x :: breadth_first (Gt (y, t1@t2));;
 ;;
-
 let breadth_first_t l = 
 	let rec aux acc = function
 		Gt (x, []) -> List.rev (x::acc)
 		| Gt (x, (Gt (y, t2))::t1) -> aux (x::acc) (Gt (y, List.rev_append (List.rev t1) t2))
 	in aux [] l
 ;;
-(*
-let t = Gt (2, List.init 300_000 (function _ -> Random.init 1_000_000));;
-*)
-
-
+let t = Gt(0, List.init 300_000 (fun x -> Gt(x, [])));;
